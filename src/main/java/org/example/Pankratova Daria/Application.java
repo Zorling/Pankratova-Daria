8922934d2bf09a93d6d904b771677c6f80df1b0d
Zorling <zorling495@mail.ru>
2021-04-05 22:06:32 +0300

    Выполнено задание 4

---
+++ src/main/java/org/example/Pankratova Daria/Application.java
@@ -0,0 +1,9 @@
+package org.example.PankratovaDaria;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Pankratova Daria");
+    }
+
+}
---
+++ src/test/java/TriangleTest/TriandlTest.java
@@ -0,0 +1,39 @@
+package TriangleTest;
+
+
+import org.junit.jupiter.api.*;
+import triangle.Triangle;
+//важный импорт, указать, что в импорте должен быть именно он
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class TriandlTest {
+    Triangle triangle = new Triangle(10,10,10);
+
+    @Test
+    @DisplayName("Проверяем, что такой треугольник существует")
+
+    void triandleCorrect(){
+        boolean check = triangle.triangleCheck();
+
+        Assertions.assertTrue(check);
+
+
+    }
+    //проверяем, что рассчет площади треугольника верен
+    @Test
+    @DisplayName("Проверяем, что рассчет площади треугольника верен")
+
+     void triandleTestArea(){
+        Assumptions.assumeTrue(triangle.triangleCheck()==true);
+        double squer = triangle.triangleArea();
+
+        Assertions.assertTrue(squer == 43.30127018922193);
+
+
+    }
+
+
+
+}
